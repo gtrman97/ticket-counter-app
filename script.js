f835c44037cd78cd8f204b61b08e283d22a3cbf0
gtrman97 <andrew.killian@g.austincc.edu>
2021-07-26 00:49:53 -0500

    improved function to store int instead of string

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function incrementValue () {
     
    var element = document.getElementById('incrementText');
-   var value = element.innerHTML; 
+   var value = parseInt(element.innerHTML);
 
     value++; 
 
     // console.log(value);
-    document.getElementById('incrementText').innerHTML = value; 
+    element.innerHTML = value; 
 } 
